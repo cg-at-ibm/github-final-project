2e3871fbfc9c9eb6cdf40dcd20ee9cededdb5a6c
cg-at-ibm <130545587+cg-at-ibm@users.noreply.github.com>
2023-04-18 23:56:14 -0700

    Update simple-interest.sh

--- simple-interest.sh
+++ simple-interest.sh
@@ -4,7 +4,7 @@
    # Do not use this in production. Sample purpose only.
    # Author: Upkar Lidder (IBM)
    # Additional Authors:
-   # <your GitHub username>
+   # cg-at-ibm
    # Input:
    # p, principal amount
    # t, time period in years
